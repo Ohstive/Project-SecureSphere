c36dd7c292a6bbe2ee3ba707ee3f95b03aca2029
MathOccit <116546294+MathOccit@users.noreply.github.com>
2023-12-11 16:21:00 +0100

    interface

---
+++ interface/InterfaceSecureSphere/JobForm.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Usb;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using InterfaceSecureSphere.Model;
+
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace InterfaceSecureSphere
+{
+
+    
+    public sealed partial class JobForm : Page, INotifyPropertyChanged
+    {
+        private InterfaceSecureSphere.Model.JobConfiguration jobConfig;
+        public event EventHandler<InterfaceSecureSphere.Model.JobConfiguration> JobConfigurationSubmitted;
+
+
+        public InterfaceSecureSphere.Model.JobConfiguration JobConfig
+        {
+            get { return jobConfig; }
+            set
+            {
+                if (jobConfig != value)
+                {
+                    jobConfig = value;
+                    OnPropertyChanged(nameof(JobConfig));
+                }
+            }
+        }
+
+        public JobForm()
+        {
+            this.InitializeComponent();
+
+            JobConfig = new InterfaceSecureSphere.Model.JobConfiguration("", "", "", ""); // Initialisez avec des valeurs par défaut
+
+        }
+
+  
+        private async void BrowseSourceButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new FolderPicker();
+            var filePicker = new FileOpenPicker();
+
+            switch (SourceTypeComboBox.SelectedIndex)
+            {
+                case 0: // Folder
+                    folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+                    folderPicker.FileTypeFilter.Add("*");
+
+                    StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+
+                    if (folder != null)
+                    {
+                        SourceTextBox.Text = folder.Path;
+                    }
+                    break;
+
+                case 1: // File
+                    filePicker.SuggestedStartLocation = PickerLocationId.Desktop;
+                    filePicker.FileTypeFilter.Add("*");
+
+                    StorageFile file = await filePicker.PickSingleFileAsync();
+
+                    if (file != null)
+                    {
+                        SourceTextBox.Text = file.Path;
+                    }
+                    break;
+            }
+        }
+
+
+        private async void BrowseTargetButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add("*");
+
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+
+            if (folder != null)
+            {
+                TargetTextBox.Text = folder.Path;
+            }
+        }
+
+        private ObservableCollection<InterfaceSecureSphere.Model.JobConfiguration> backupJobs = new ObservableCollection<InterfaceSecureSphere.Model.JobConfiguration>();
+        private void OnJobConfigurationSubmitted(InterfaceSecureSphere.Model.JobConfiguration jobConfig)
+        {
+            JobConfigurationSubmitted?.Invoke(this, jobConfig);
+            Debug.WriteLine("Job configuration submitted.");
+
+        }
+
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string jobName = JobConfig.JobName;
+            string sourceType = ((ComboBoxItem)SourceTypeComboBox.SelectedItem)?.Content.ToString();
+            string sourcePath = SourceTextBox.Text;
+            string targetPath = TargetTextBox.Text;
+            string backupType = ((ComboBoxItem)TypeComboBox.SelectedItem)?.Content.ToString();
+
+            if (jobName != "" && sourceType != "" && sourcePath != "" && targetPath != "" && backupType != "")
+            {
+                // Create a new job configuration
+                var submittedJobConfig = new InterfaceSecureSphere.Model.JobConfiguration(jobName, sourcePath, targetPath, backupType);
+
+                // add the job to the list of jobs
+                ((App)Application.Current).BackupJobs.Add(submittedJobConfig);
+
+                Frame.GoBack();
+
+                System.Diagnostics.Debug.WriteLine($"JobName: {jobName}, SourceType: {sourceType}, Source: {sourcePath}, Target: {targetPath}, BackupType: {backupType}");
+            }
+         
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ interface/InterfaceSecureSphere/LogsMenuPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace InterfaceSecureSphere
+{
+    public sealed partial class LogsMenuPage : Page
+    {
+        public string LogFolderPath { get; set; }
+
+        public LogsMenuPage()
+        {
+            this.InitializeComponent();
+
+            // Charger la valeur depuis les paramètres de l'application
+            LoadLogFolderPath();
+            txtLogFolderPath.Text = LogFolderPath;
+        }
+
+        private void LoadLogFolderPath()
+        {
+            // Charger la valeur depuis les paramètres de l'application
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            if (localSettings.Values.TryGetValue("LogFolderPath", out object logFolderPath))
+            {
+                LogFolderPath = logFolderPath.ToString();
+            }
+            else
+            {
+                // Valeur par défaut si la clé n'est pas trouvée
+                LogFolderPath = "C:\\Logs";
+            }
+        }
+
+        private void SaveLogFolderPath()
+        {
+            // Sauvegarde de la valeur dans les paramètres de l'application
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values["LogFolderPath"] = LogFolderPath;
+        }
+
+        private async void BrowseButtonClick(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new FolderPicker
+            {
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+            };
+
+            // Afficher le FolderPicker et obtenir le dossier sélectionné
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+
+            // Afficher le chemin du dossier sélectionné
+            if (folder != null)
+            {
+                LogFolderPath = folder.Path;
+                txtLogFolderPath.Text = LogFolderPath;
+            }
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Sauvegarde de la nouvelle valeur du chemin du dossier des journaux
+            SaveLogFolderPath();
+            Console.WriteLine($"Chemin du dossier des journaux enregistré : {LogFolderPath}");
+        }
+
+        // Appelé lorsque la navigation vers une autre page échoue
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            // Sauvegarde de la valeur lorsque la page est quittée
+            SaveLogFolderPath();
+            base.OnNavigatingFrom(e);
+        }
+    }
+}
