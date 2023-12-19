1dcb47637f2cf0b0248730d128d92341b9e06e1a
Ohstive <119133853+Ohstive@users.noreply.github.com>
2023-12-19 21:03:09 +0100

    Add files via upload

---
+++ SecureSphereV3/Model/JobConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SecureSphereV2.Model
+{
+    public class JobConfiguration
+    {
+        // Propriétés de la tâche
+        public string JobName { get; set; } // Rendre l'accès en écriture public
+        public string SourceDirectoryPath { get; set; }
+        public bool IsSourceDirectory { get; set; }
+        public string TargetDirectoryPath { get; set; }
+        public string BackupType { get; set; }
+        public string EncryptionKey { get; set; }
+        public bool IsEncryptionEnabled { get; set; }
+        public string Encryption { get; set; }
+        public bool IsFinished { get; set; }
+
+        // Constructeur
+        public JobConfiguration(string name, string source, bool isSourceDirectory, string target, string backupType, string encryptionKey, bool isEncryptionEnabled)
+        {
+            JobName = name;
+            SourceDirectoryPath = source;
+            IsSourceDirectory = isSourceDirectory;
+            TargetDirectoryPath = target;
+            BackupType = backupType;
+            EncryptionKey = encryptionKey;
+            IsEncryptionEnabled = isEncryptionEnabled;
+
+            if (IsEncryptionEnabled)
+            {
+                Encryption = "Enabled";
+            }
+            else
+            {
+                Encryption = "Disabled";
+            }
+
+        }
+    }
+}
