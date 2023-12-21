fdc69dbbf95b992ee8c8091a39595bbef5ea1ba4
Shiva-123456789 <151729154+Shiva-123456789@users.noreply.github.com>
2023-12-21 16:34:19 +0100

    Version 3.0
    Version 3.0

---
+++ SecureSphereV3/DistanceAcess/ServerSocketManager.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Security;
+using System.Threading;
+
+namespace SecureSphereV2
+{
+    public class ServerSocketManager
+    {
+        private TcpListener tcpListener;
+        private Thread listenerThread;
+
+        public void Start()
+        {
+            tcpListener = new TcpListener(System.Net.IPAddress.Any, 63774);
+            listenerThread = new Thread(ListenForClients);
+            listenerThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+                Thread clientThread = new Thread(HandleClient);
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClient(object clientObj)
+        {
+            TcpClient tcpClient = (TcpClient)clientObj;
+
+            try
+            {
+                using (NetworkStream stream = tcpClient.GetStream())
+                {
+                    byte[] data = new byte[256];
+                    int bytesRead = stream.Read(data, 0, data.Length);
+                    string clientData = System.Text.Encoding.ASCII.GetString(data, 0, bytesRead).Trim();
+
+                    // Vérifiez si le mot de passe est fourni (non vide)
+                    if (string.IsNullOrWhiteSpace(clientData))
+                    {
+                        // Aucun mot de passe fourni, autoriser la connexion
+                        SendJobList(stream);
+                    }
+                    else if (IsCorrectPassword(clientData))
+                    {
+                        // Password is correct, perform further actions
+                        SendJobList(stream);
+                    }
+                    else
+                    {
+                        // Password is incorrect
+                        Console.WriteLine("Incorrect password.");
+                        // Inform the client about the incorrect password
+                        byte[] response = System.Text.Encoding.ASCII.GetBytes("INCORRECT_PASSWORD");
+                        stream.Write(response, 0, response.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error handling client: {ex.Message}");
+            }
+            finally
+            {
+                tcpClient.Close();
+            }
+        }
+
+        private void SendJobList(NetworkStream stream)
+        {
+            // Convert the list of jobs to a JSON string (use a JSON serialization library like Newtonsoft.Json)
+            string jobsJson = "[\"Job1\", \"Job2\", \"Job3\"]"; // Replace with your own logic
+
+            byte[] response = System.Text.Encoding.ASCII.GetBytes(jobsJson);
+            stream.Write(response, 0, response.Length);
+        }
+
+        private bool IsCorrectPassword(string enteredPassword)
+        {
+            // Replace this with your actual logic to validate the password
+            SecureString savedPassword = GetSecureSavedPassword();
+            return SecureStringEqual(savedPassword, SecureStringFromPlainText(enteredPassword));
+        }
+
+        private SecureString GetSecureSavedPassword()
+        {
+            // Replace this with your actual logic to retrieve the saved password securely
+            return SecureStringFromPlainText("YourStoredSecurePassword");
+        }
+
+        private SecureString SecureStringFromPlainText(string plainText)
+        {
+            SecureString secureString = new SecureString();
+            foreach (char c in plainText)
+            {
+                secureString.AppendChar(c);
+            }
+            return secureString;
+        }
+
+        private bool SecureStringEqual(SecureString s1, SecureString s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+
+            IntPtr bstr1 = IntPtr.Zero;
+            IntPtr bstr2 = IntPtr.Zero;
+
+            try
+            {
+                bstr1 = System.Runtime.InteropServices.Marshal.SecureStringToBSTR(s1);
+                bstr2 = System.Runtime.InteropServices.Marshal.SecureStringToBSTR(s2);
+
+                return string.Equals(System.Runtime.InteropServices.Marshal.PtrToStringBSTR(bstr1), System.Runtime.InteropServices.Marshal.PtrToStringBSTR(bstr2), StringComparison.Ordinal);
+            }
+            finally
+            {
+                if (bstr1 != IntPtr.Zero)
+                    System.Runtime.InteropServices.Marshal.ZeroFreeBSTR(bstr1);
+
+                if (bstr2 != IntPtr.Zero)
+                    System.Runtime.InteropServices.Marshal.ZeroFreeBSTR(bstr2);
+            }
+        }
+    }
+}
---
+++ SecureSphereV3/ViewModel/Services/CopyFile/CopyWithProgress.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using SecureSphereV2.ViewModel.Services.Log;
+using System;
+using System.Windows;
+using System.Threading.Tasks;
+using SecureSphereV2.ViewModel.Services.Progress;
+
+
+namespace SecureSphereV2.ViewModel.Services.CopyFile
+{
+   
+    internal class CopyWithProgress:ICopyFileHandler
+    {
+        private readonly ILoger _log;
+
+        public CopyWithProgress(ILoger log)
+        {
+               _log = log;
+        }
+
+
+        public void DifferentialCopy(string CopieName, string sourcePath, string targetPath, FileInfo file)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void FullCopy(string CopieName, string sourcePath, string targetPath, FileInfo file)
+        {
+            System.Windows.MessageBox.Show("ProgressCopy");
+
+        }
+
+
+        private async Task CopyFileAsync(string sourcePath, string destinationDirectory, IProgress<CopyProgress> progress)
+        {
+            string destinationPath = Path.Combine(destinationDirectory, Path.GetFileName(sourcePath));
+
+            using (var sourceStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
+            using (var destinationStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write))
+            {
+                var buffer = new byte[327680]; // 320 KB buffer (adjust as needed)
+                int bytesRead;
+                long totalBytesRead = 0;
+                long fileSizeInBytes = sourceStream.Length;
+
+                var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+                while ((bytesRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    await destinationStream.WriteAsync(buffer, 0, bytesRead);
+                    totalBytesRead += bytesRead;
+
+                    // Report progress
+                    double speed = totalBytesRead / stopwatch.Elapsed.TotalSeconds;
+                    double remainingTime = (fileSizeInBytes - totalBytesRead) / speed;
+                    int progressPercentage = (int)((totalBytesRead * 100) / fileSizeInBytes);
+
+                    // Report state
+                    var copyProgress = new CopyProgress(speed, remainingTime, progressPercentage, CopyState.InProgress, fileSizeInBytes, totalBytesRead);
+                    progress.Report(copyProgress);
+
+                    // Pause/Resume handling
+                    /*
+                    while (isPaused)
+                    {
+                        await Task.Delay(100);
+                    }
+
+                    if (isStopped)
+                    {
+                        break;
+                    }
+                    */
+                }
+                // Report completion
+                var completionProgress = new CopyProgress(0, 0, 100, CopyState.Completed, fileSizeInBytes, totalBytesRead);
+                progress.Report(completionProgress);
+            }
+        }
+
+        public string FormatBytes(double bytes)
+        {
+            string[] suffixes = { "B", "KB", "MB", "GB", "TB" };
+            int suffixIndex = 0;
+
+            while (bytes >= 1024 && suffixIndex < suffixes.Length - 1)
+            {
+                bytes /= 1024;
+                suffixIndex++;
+            }
+
+            return $"{bytes:N2} {suffixes[suffixIndex]}/s";
+        }
+
+
+    }
+}
